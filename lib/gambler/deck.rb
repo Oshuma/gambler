bfa8360fb952bc2c63b2874892e1d08e72c3d1f3
Dale Campbell <dale@save-state.net>
2008-06-06 05:34:09 -0500

    More test for Deck.

--- lib/gambler/deck.rb
+++ lib/gambler/deck.rb
@@ -11,18 +11,32 @@ module Gambler
   #   Deck.new(:size => 20)
   #
   # A Deck containing the specified +cards+; +size+ will be set to <tt>cards.size</tt>.
-  #   Deck.new(:cards => ['As', 'Ks', 'Qs', 'Js', 'Ts'])
-  #
-  # A Deck containing diamonds only.
-  #   Deck.new(:cards => :diamonds)
+  #   Deck.new(:cards => Card.diamonds)
   class Deck
     DEFAULT_CARDS = Card.all
     DEFAULT_SIZE  = DEFAULT_CARDS.size
 
+    attr_accessor :cards
+
     def initialize(options = {})
-      @cards = options[:cards] || DEFAULT_CARDS
-      @size  = options[:size]  || DEFAULT_SIZE
+      options[:cards] ||= DEFAULT_CARDS
+      options[:size]  ||= DEFAULT_SIZE
+
+      options[:cards].each do |card|
+        raise InvalidDeck unless card.is_a? Card
+      end
+      raise InvalidDeckSize unless options[:size].is_a? Fixnum
+
+      @cards, @size = options[:cards], options[:size]
+    end
+
+    # Shuffles the Deck; changes <tt>@cards</tt> to reflect the shuffle.
+    def shuffle!
+      @cards.sort! {rand}
     end
   end # of Deck
+
+  class InvalidDeck < Exception; end
+  class InvalidDeckSize < Exception; end
 
 end # of Gambler
--- test/test_deck.rb
+++ test/test_deck.rb
@@ -3,6 +3,31 @@ require File.dirname(__FILE__) + '/helper'
 # Tests the Deck class.
 class TestDeck < Test::Unit::TestCase
   def test_new_deck
-    assert_kind_of(Deck, Deck.new)
+    deck = Deck.new
+    assert_kind_of(Deck, deck)
+    assert_equal(52, deck.cards.size)
+    assert_kind_of(Array, deck.cards)
+    deck.cards.each do |card|
+      assert_kind_of(Card, card)
+    end
+  end
+
+  def test_invalid_cards
+    assert_raise(InvalidDeck) do
+      Deck.new(:cards => 'WTF')
+    end
+  end
+
+  def test_invalid_deck_size
+    assert_raise(InvalidDeckSize) do
+      Deck.new(:size => 'WTF')
+    end
+  end
+
+  def test_shuffle!
+    deck = Deck.new
+    first_card = deck.cards.first
+    deck.shuffle!
+    assert_not_equal(first_card, deck.cards.first)
   end
 end
