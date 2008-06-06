6ad9fa91c1d5bc1901f1a74ff40aaee5c5254d57
Dale Campbell <dale@save-state.net>
2008-06-06 09:30:45 -0500

    Test for InvalidPlayerSize

--- test/game/test_blackjack.rb
+++ test/game/test_blackjack.rb
@@ -7,13 +7,20 @@ class TestBlackjack < Test::Unit::TestCase
   def setup
     @dale  = Player.new('Dale')
     @kenny = Player.new('Kenny')
+    @game = Blackjack.new(:players => [@dale, @kenny])
   end
 
   def test_new_blackjack_game
-    @game = Blackjack.new(:players => [@dale, @kenny])
     assert_kind_of(Blackjack, @game)
     assert_equal(10, @game.ante)
     assert_equal(0, @game.pot)
     assert_kind_of(Deck, @game.deck)
+  end
+
+  def test_invalid_player_size
+    assert_raise(InvalidPlayerSize) do
+      Blackjack.new(:players => [])
+      Blackjack.new(:players => [@dale])
+    end
   end
 end
