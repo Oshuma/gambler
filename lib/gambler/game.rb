dd63add86793b814e680839b57b92f9d84e01663
Dale Campbell <dale@save-state.net>
2008-06-06 07:42:38 -0500

    Moving stuff into BasicGame.

--- lib/gambler/game.rb
+++ lib/gambler/game.rb
@@ -1,9 +1,12 @@
+require 'gambler/game/basic_game'
 require 'gambler/game/blackjack'
 
 module Gambler
-  
+
   module Game
+    class InvalidPlayers    < Exception; end
     class InvalidPlayerSize < Exception; end
+    class NoPlayers         < Exception; end
   end # of Game
-  
+
 end # of Gambler
--- lib/gambler/game/blackjack.rb
+++ lib/gambler/game/blackjack.rb
@@ -3,30 +3,11 @@ module Gambler
   module Game
 
     # The Game of Blackjack.
-    #
-    # Example:
-    #   @dale  = Player.new('Dale')
-    #   @kenny = Player.new('Kenny')
-    #   options = {
-    #     :players => [@dale, @kenny],
-    #     :ante => 10,
-    #     :pot => 1_000
-    #   }
-    #   Blackjack.new(options)
-    class Blackjack
-      INITIAL_ANTE = 10
-      INITIAL_POT  = 0
-
-      attr_reader :players
-      attr_accessor :ante, :deck, :pot
+    class Blackjack < Gambler::Game::BasicGame
 
       def initialize(options = {})
         raise InvalidPlayerSize unless options[:players].size >= 2
-        @players = options[:players]
-        @deck = Deck.new
-        3.times { @deck.shuffle! }
-        @ante = options[:ante] || INITIAL_ANTE
-        @pot  = options[:pot]  || INITIAL_POT
+        super(options)
       end
     end # of Blackjack
 
