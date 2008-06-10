c60be98aabb64d8c7643bc8e047ccf9e165094c3
Dale Campbell <dale@save-state.net>
2008-06-10 16:04:27 -0500

    Placeholders for the Client tests.

--- test/client/test_client.rb
+++ test/client/test_client.rb
@@ -2,11 +2,7 @@ require File.dirname(__FILE__) + '/../helper'
 
 # Tests Gambler::Client
 class TestClient < Test::Unit::TestCase
-  def setup
-    @client = Client.new
-  end
-
-  def test_new_client
-    assert_kind_of(Client, @client)
+  def test_truth
+    assert true
   end
 end
--- test/client/test_client_helper.rb
+++ test/client/test_client_helper.rb
@@ -2,4 +2,7 @@ require File.dirname(__FILE__) + '/../helper'
 
 # Tests Gambler::ClientHelper
 class TestClientHelper < Test::Unit::TestCase
+  def test_truth
+    assert true
+  end
 end
